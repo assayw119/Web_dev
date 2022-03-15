1fb0a4adafbd06dfead035f9442cc94b0aecea7a
YoungWoong Park <assayw119@naver.com>
2022-03-16 01:48:04 +0900

    5주차
    AWS 서버 연동을 위한 클라이언트 변경

--- homework/app.py
+++ homework/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
+db = client.dbhomework
+
+
+## HTML 화면 보여주기
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('index.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5001, debug=True)
